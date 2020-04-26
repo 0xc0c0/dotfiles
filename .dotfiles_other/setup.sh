4858ce3e24ee3afdd84214c3464c5b99de71a6c9
c0c0 <bnheruska@gmail.com>
2020-04-26 15:00:40 -0400

    Update setup.sh

--- .dotfiles_other/setup.sh
+++ .dotfiles_other/setup.sh
@@ -3,7 +3,7 @@
 alias dotfiles="$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 TMP=$(mktemp -d)
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/0xc0c0/dotfiles.git $TMP
-rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rsync --recursive --verbose --exclude '.git' $TMP/ $HOME/
 rm -r $TMP
 dotfiles config --local status.showUntrackedFiles no
 dotfiles ls-tree -r master --name-only $HOME
