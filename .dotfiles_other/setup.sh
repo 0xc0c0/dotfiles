4eb746b86d43b7d2fa2af32d4238954ea9a33f98
c0c0 <c0c0@benslab.org>
2021-01-31 20:29:22 -0600

    Update setup.sh
    syntax updates

--- .dotfiles_other/setup.sh
+++ .dotfiles_other/setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-alias dotfiles="$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias dotfiles="$(which git) --git-dir=${HOME}/.dotfiles/ --work-tree=${HOME}"
 TMP=$(mktemp -d)
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/0xc0c0/dotfiles.git $TMP
+git clone --separate-git-dir=${HOME}/.dotfiles https://github.com/0xc0c0/dotfiles.git ${TMP}
 [[ -z $(which rsync) ]] && sudo apt-get install rsync -y
-rsync --recursive --verbose --exclude '.git' $TMP/ $HOME/
-rm -r $TMP
+rsync --recursive --verbose --exclude '.git' ${TMP}/ ${HOME}/
+rm -r ${TMP}
 dotfiles config --add status.showUntrackedFiles no
-dotfiles ls-tree -r master --name-only $HOME
+dotfiles ls-tree -r master --name-only ${HOME}
 dotfiles status
