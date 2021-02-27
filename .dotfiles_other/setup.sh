1f77ae2bc073c58859b5f964890e7aa139cf4604
c0c0 <c0c0@benslab.org>
2021-02-26 19:46:47 -0600

    Update setup.sh

--- .dotfiles_other/setup.sh
+++ .dotfiles_other/setup.sh
@@ -11,7 +11,7 @@ git clone --separate-git-dir=${HOME}/.dotfiles --recurse-submodules https://gith
 [[ -z $(which rsync) ]] && sudo apt-get install rsync -y
 rsync --recursive --verbose --exclude '.git' ${TMP}/ ${HOME}/
 rm -r ${TMP}
-source ${HOME}/.aliases
+. .aliases
 dotfiles config --add status.showUntrackedFiles no
 dotfiles ls-tree -r master --name-only ${HOME}
 dotfiles status
